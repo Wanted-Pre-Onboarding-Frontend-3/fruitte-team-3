49a0ed7d65ca884fc7ed3f7772df2b46bebf5e16
Lihu Kim <lh.ines.kim@gmail.com>
2022-09-03 00:06:01 +0900

    design: GlobalStyles 기본값 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Gnb from './components/Gnb';
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Gnb />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
