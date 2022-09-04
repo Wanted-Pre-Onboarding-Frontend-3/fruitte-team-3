67cac942e40bef701229f1a2b8b0b72ac7f0ef20
Lihu Kim <lh.ines.kim@gmail.com>
2022-09-04 23:17:45 +0900

    fix: 기본 경로 수정
    기본 경로 진입시 상품목록 노출

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import OrderComplete from './components/Order/OrderComplete';
 import ProductRegister from './components/ProductRegister';
-import Home from './pages/home';
 import Order from './pages/Order/Order';
 import OrderHistory from './pages/OrderHistory/OrderHistory';
 import OrderHistoryDetail from './pages/OrderHistory/OrderHistoryDetail';
@@ -16,15 +15,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/fruitstore" element={<ProductList />} />
+          <Route path="/" element={<ProductList />} />
           <Route path="/fruitstore/*" element={<ProductDetail />} />
           <Route path="/admin/product-manage" element={<ProductManage />} />
           <Route path="/admin/product-register" element={<ProductRegister />} />
           <Route path="/order" element={<Order />} />
           <Route path="/order-complete" element={<OrderComplete />} />
           <Route path="/mypage/order/:id" element={<OrderHistoryDetail />} />
-          <Route path="/mypage/order" element={<OrderHistory />} />                  
+          <Route path="/mypage/order" element={<OrderHistory />} />
         </Route>
       </Routes>
     </BrowserRouter>
