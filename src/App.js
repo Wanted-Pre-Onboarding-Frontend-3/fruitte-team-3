6eea92eb6eb6fb89fbdad6091f72e58400f3768c
Lihu Kim <lh.ines.kim@gmail.com>
2022-09-04 20:36:21 +0900

    update: 상품등록 페이지, 로고이미지 경로 변경

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import OrderComplete from './components/Order/OrderComplete';
+import ProductRegister from './components/ProductRegister';
 import Home from './pages/home';
 import Order from './pages/Order';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
@@ -17,6 +18,7 @@ function App() {
           <Route path="/fruitstore" element={<ProductList />} />
           <Route path="/fruitstore/*" element={<ProductDetail />} />
           <Route path="/admin/product-manage" element={<ProductManage />} />
+          <Route path="/admin/product-register" element={<ProductRegister />} />
           <Route path="/order" element={<Order />} />
           <Route path="/order-complete" element={<OrderComplete />} />
         </Route>
