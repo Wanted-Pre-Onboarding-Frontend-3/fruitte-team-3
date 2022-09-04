d18c15c2327bb6e55d1d80ba40a2f65cfeb127db
ycs1m1yk <satoly4@gmail.com>
2022-09-04 21:02:43 +0900

    feat: 상품목록 관리 페이지 스타일링 및 선택삭제 버튼 추가

--- src/pages/productManage/index.jsx
+++ src/pages/productManage/index.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { BsTrashFill } from 'react-icons/bs';
+import styled from 'styled-components';
 
+import { fonts } from '../../styles/fonts';
 import TableWithCheckbox from './components/tableWithCheckbox';
 
 const tableConfig = {
-  colSizes: [6, 40, 30, 12, 12],
+  colSizes: [6, 10, 50, 17, 17],
   fieldNames: ['상품ID', '상품명', '노출 여부', '삭제하기'],
 };
 export default function ProductManage() {
@@ -20,12 +22,20 @@ export default function ProductManage() {
     }
   };
 
+  const handleDeleteSelected = () => {
+    if (window.confirm('선택한 상품들을 삭제하시겠습니까?')) {
+      alert(`선택한 상품들이 삭제되었습니다.`);
+    }
+  };
+
   const handleProductDisplayToggle = (e) => {
     const isChecked = e.target.checked;
     if (window.confirm('노출 여부를 변경하시겠습니까?')) {
       const { itemId } = e.target.closest('tr[data-item-id]')?.dataset;
       if (itemId) {
-        const message = isChecked ? '가 노출됩니다.' : '가 노출되지 않습니다.';
+        const message = isChecked
+          ? '(이)가 노출됩니다.'
+          : '(이)가 노출되지 않습니다.';
         setTimeout(() => alert(`상품 ${itemId} ${message}`), 100);
       }
     } else {
@@ -34,10 +44,10 @@ export default function ProductManage() {
   };
 
   const handleProductDelete = (e) => {
-    if (window.confirm('정말로 상품을 삭제하시겠습니까?')) {
+    if (window.confirm('상품을 삭제하시겠습니까?')) {
       const { itemId } = e.target.closest('tr[data-item-id]')?.dataset;
       if (itemId) {
-        alert(`상품 ${itemId} 가 삭제되었습니다.`);
+        alert(`상품 ${itemId} (이)가 삭제되었습니다.`);
       }
     }
   };
@@ -64,12 +74,29 @@ export default function ProductManage() {
   }, []);
 
   return (
-    <>
-      <h1>상품목록 관리 페이지</h1>
+    <Container>
+      <Title>상품목록 관리</Title>
+      <DeleteButton onClick={handleDeleteSelected}>선택삭제</DeleteButton>
       <TableWithCheckbox
         data={productList.map(mapProductToManageTable)}
         config={tableConfig}
       />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  padding: 20px 0;
+  gap: 20px;
+`;
+
+const Title = styled.h1`
+  ${fonts.H1};
+`;
+
+const DeleteButton = styled.button`
+  width: fit-content;
+`;
